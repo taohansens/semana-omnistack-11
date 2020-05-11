d05030e2976b0d561952a02ebbccd2eb7b74dd49
Tao Hansen <tao.santos@somosicev.com>
2020-05-10 22:04:11 -0300

    css added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,9 @@
 import React, {useState} from 'react';
 
-import Header from './Header';
+import Logon from './pages/Logon';
 function App() {
-  const [counter, setCounter] = useState(0);
-  //retorna um array [valor, função de atualização]
-  
-  function increment() {
-    setCounter(counter + 1);
-  }
-  
   return (
-    <div>
-    <Header>Contador: {counter}</Header>
-    <button onClick={increment}>Incrementar</button>
-    </div>
+    <Logon />
   );
 }
 
