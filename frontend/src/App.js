5dd43723bc9fc50f4ba7c2cc59189b3701d32491
Tao Hansen <tao.santos@somosicev.com>
2020-05-10 21:52:15 -0300

    conceito de Estado e imutabilidade, add logo e ima

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import Header from './Header';
 function App() {
+  const [counter, setCounter] = useState(0);
+  //retorna um array [valor, função de atualização]
+  
+  function increment() {
+    setCounter(counter + 1);
+  }
+  
   return (
-    <Header title="Semana Omnistack" />
+    <div>
+    <Header>Contador: {counter}</Header>
+    <button onClick={increment}>Incrementar</button>
+    </div>
   );
 }
 
