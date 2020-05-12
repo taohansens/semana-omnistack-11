32c3e6328ad4f5c090c784cd9c1ae62b62f77b25
Tao Hansen <tao.santos@somosicev.com>
2020-05-11 22:54:00 -0300

    login ok. create register routes dom

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,9 +1,31 @@
 import React from 'react';
-
+import {FiLogIn, FiPrinter} from 'react-icons/fi'
+import {Link} from 'react-router-dom'; //Comportamento SPA.
 import './styles.css';
+
+import logoImg from '../../assets/logo.svg'
+import heroesImg from '../../assets/heroes.png';
 
 export default function Logon() {
     return(
-        <h1>Futuro Tina...</h1>
+        <div className="logon-container">
+        <section className="form">
+            <img src={logoImg} alt="Be The Hero" />
+
+            <form>
+                <h1>Faça seu logon</h1>
+                <input placeholder="Sua ID"/>
+                <button className="button">Entrar</button>
+                <Link to="/register">
+                    <FiLogIn size={16} color="#E02041"/>
+                Não tenho cadastro.</Link>
+            </form>
+
+           </section>
+
+           <img src={heroesImg} alt="Heroes" />
+        </div>
+        
     );
 }
+
