313c92900e82e804ee6845d32c8a15fde5d8e293
Tao Hansen <tao.santos@somosicev.com>
2020-05-13 19:02:44 -0300

    register ok integrated with node

--- frontend/src/pages/Register/index.js
+++ frontend/src/pages/Register/index.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import {Link} from 'react-router-dom';
+import {Link, useHistory} from 'react-router-dom';
 import api from '../../services/api';
 import './styles.css';
 import LogoImg from '../../assets/logo.svg';
@@ -12,15 +12,26 @@ export default function Register() {
     const [city, setCity] = useState('');
     const [uf, setUf] = useState('');
 
-    function handleRegister(e){
+    const history = useHistory();
+
+    async function handleRegister(e){
         e.preventDefault();
-        console.log({
+        const data= {
            name,
            email,
            whatsapp,
            city,
            uf, 
-        })
+        };
+        
+        try {
+        const response = await api.post('ongs', data);
+
+        alert(`Seu ID de acesso: ${response.data.id}`);
+        history.push('/');
+        } catch(err) {
+        alert('Erro no cadastro, tente novamente.')
+        }
     }
 
     return (
