917313bf32a6dae0036455b97f1f747da0e60a49
Tao Hansen <tao.santos@somosicev.com>
2020-05-13 18:19:56 -0300

    finished frontend

--- frontend/src/pages/NewIncident/index.js
+++ frontend/src/pages/NewIncident/index.js
@@ -1,7 +1,30 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
+
 
 import './styles.css';
+import LogoImg from '../../assets/logo.svg';
+import {FiArrowLeft} from 'react-icons/fi'
 
 export default function NewIncident() {
-    return <h1>NewIncident</h1>
+    return (
+        <div className="new-incident-container">
+            <div className="content">
+                <section>
+                    <img src={LogoImg} alt = "Be The Hero"/>
+                    <h1>Cadastrar novo caso</h1>
+                    <p>Descreva o caso detalhadamente para encontrar um herói para resolver isso.</p>
+
+                    <Link className="back-link" to="/profile">
+                    <FiArrowLeft size={16} color="#E02041"/>Voltar para home.</Link>
+                </section>
+                <form>
+                    <input placeholder="Título do caso" />
+                    <textarea placeholder="Descrição" />
+                    <input placeholder="Valor em reais" />
+                    <button className="button" type="submit">Cadastrar</button>
+                </form>
+            </div>
+        </div>
+    )
 }
