2206d3daa9621cb499c3b0557d30fb45d203d65f
Tao Hansen <tao.santos@somosicev.com>
2020-07-01 21:27:57 -0300

    finalizado cadastro de incidentes + exclusão de incidentes. finalizado

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from 'react';
-import {Link} from 'react-router-dom';
+import {Link, useHistory} from 'react-router-dom';
 import {FiPower, FiTrash2} from 'react-icons/fi'
 import logoImg from '../../assets/logo.svg';
 
@@ -12,7 +12,7 @@ export default function Profile() {
 
     const ongName = localStorage.getItem('ongName');
     const ongId = localStorage.getItem('ongId');
-
+    const history = useHistory();
     useEffect(() => {
         api.get('profile', {
             headers: {
@@ -25,15 +25,22 @@ export default function Profile() {
 
     async function handleDeleteIncident(id){
         try {
-            await api.delete(`incidents/{$id}`, {
+            await api.delete(`incidents/${id}`, {
             headers: {
                 Authorization: ongId,
             }
             });
+
+        setIncidents(incidents.filter(incident => incident.id !== id));
         } catch (err) {
             alert('Erro ao deletar caso, tente novamente.');
         }
         }
+    function handleLogout(){
+        localStorage.clear();
+        history.push('/');
+
+    }    
 
     return (
         <div className='profile-container'>
@@ -42,7 +49,7 @@ export default function Profile() {
                 <span>Bem vinda, {ongName}</span>
 
                 <Link className="button" to='/incidents/new'>Cadastrar novo caso</Link>
-                <button type="button">
+                <button onClick={handleLogout} type="button">
                     <FiPower size={18} color="#E02041" />
                 </button>
             </header>
