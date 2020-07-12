324de4fa5600ed8fb24e1b0797c66f8671797f5c
Tao Hansen <tao.santos@somosicev.com>
2020-07-12 18:31:04 -0300

    validation with celebrate. joi

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const routes = express.Router();
+const { celebrate, Segments, Joi } = require('celebrate');
 
 const OngController = require('./controllers/OngController');
 const IncidentController = require('./controllers/IncidentController');
@@ -11,7 +12,19 @@ routes.get('/profile', ProfileController.index);
 
 //Rota GET e POST das ONGS
 routes.get('/ongs', OngController.index);
-routes.post('/ongs', OngController.create);
+
+routes.post('/ongs',
+celebrate({
+    [Segments.BODY]: Joi.object().keys({
+        name: Joi.string().required(),
+        email: Joi.string().required().email(),
+        whatsapp: Joi.number().required().min(10).max(11),
+        city: Joi.string().required(),
+        uf: Joi.string().required().length(2),
+    })
+
+}),
+OngController.create);
 
 //Rota GET/POST/DELETE dos Incidentes
 routes.get('/incidents', IncidentController.index);
