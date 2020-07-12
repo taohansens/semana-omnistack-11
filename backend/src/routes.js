9d379ced6efa8c1658e6d1c47e9d6122499e75c7
Tao Hansen <tao.santos@somosicev.com>
2020-07-12 18:40:41 -0300

    validation to remove and headers (Authorization)

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -8,28 +8,36 @@ const ProfileController = require('./controllers/ProfileController');
 const SessionController = require('./controllers/SessionControlller');
 
 // Rota Profile
-routes.get('/profile', ProfileController.index);
+// https://github.com/arb/celebrate, metodos Authorization
+routes.get('/profile', celebrate({
+    [Segments.HEADERS]: Joi.object({
+        authorization: Joi.string().required(),
+    }).unknown(), 
+}), ProfileController.index);
 
 //Rota GET e POST das ONGS
 routes.get('/ongs', OngController.index);
 
 routes.post('/ongs',
-celebrate({
-    [Segments.BODY]: Joi.object().keys({
-        name: Joi.string().required(),
-        email: Joi.string().required().email(),
-        whatsapp: Joi.number().required().min(10).max(11),
-        city: Joi.string().required(),
-        uf: Joi.string().required().length(2),
-    })
-
-}),
+    celebrate({
+        [Segments.BODY]: Joi.object().keys({
+            name: Joi.string().required(),
+            email: Joi.string().required().email(),
+            whatsapp: Joi.number().required().min(10).max(11),
+            city: Joi.string().required(),
+            uf: Joi.string().required().length(2),
+            })}),
 OngController.create);
 
 //Rota GET/POST/DELETE dos Incidentes
 routes.get('/incidents', IncidentController.index);
 routes.post( '/incidents', IncidentController.create);
-routes.delete('/incidents/:id', IncidentController.delete);
+
+routes.delete('/incidents/:id', celebrate({
+    [Segments.PARAMS]: Joi.object().keys({
+        id: Joi.number().required(),
+    })
+}), IncidentController.delete);
 
 //Rota Sessão/LOGIN
 routes.post('/sessions', SessionController.create);
