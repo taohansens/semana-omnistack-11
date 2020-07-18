62a90448ef76b6030c9d450c0905d59c894cffae
Tao Hansen <tao.santos@somosicev.com>
2020-07-18 19:01:30 -0300

    conf ambiente de teste

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -16,6 +16,17 @@ module.exports = {
   useNullAsDefault: true
 },
 
+test: {
+  client: 'sqlite3',
+  connection: {
+    filename: './src/database/test.sqlite'
+},
+migrations: {
+  directory: './src/database/migrations'
+},
+useNullAsDefault: true
+},
+
 /**WEB
   development: {
     client: 'sqlite3',
