7fb96e6298d630fe8c1d902675bc40183bfb46cf
Tao Hansen <tao.santos@somosicev.com>
2020-07-18 19:22:10 -0300

    jest ok

--- backend/tests/integration/ong.spec.js
+++ backend/tests/integration/ong.spec.js
@@ -1,9 +1,23 @@
-const { intersect } = require("../../src/database/connection")
-
 const request = require('supertest');
+const app = require('../../src/app');
+const connection = require('../../src/database/connection');
 
 describe('ONG',() => {
-    it('should be able to create a new ONG',() => {
-        ''
+    beforeEach(async () => {
+        await connection.migrate.latest();
+    });
+
+    it('should be able to create a new ONG', async () => {
+        const response = await request(app)
+        .post('/ongs')
+            .send({
+            name: "Teste1",
+            email: "contato@contato.com",
+            whatsapp: "0885462254",
+            city: "Centro de Teste",
+            uf: "TE"
+            });
+
+            console.log(response.body);
     })
 })
