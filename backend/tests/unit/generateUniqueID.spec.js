b802268c8417ff6c7a82f7256123f36dd33314c7
Tao Hansen <tao.santos@somosicev.com>
2020-07-18 18:56:01 -0300

    file of test. JEST

---
+++ backend/tests/unit/generateUniqueID.spec.js
@@ -0,0 +1,8 @@
+const { intersect } = require("../../src/database/connection")
+
+//npm test
+describe('Generate Unique ID', () => {
+    it('should generate an unique ID', () =>{
+    expect(2 + 2).toBe(4);
+    });
+});
