58268494906e9f824a8f27987295ade06034f621
Tao Hansen <tao.santos@somosicev.com>
2020-07-18 19:06:02 -0300

    config dev test

---
+++ backend/tests/integration/ong.spec.js
@@ -0,0 +1,9 @@
+const { intersect } = require("../../src/database/connection")
+
+const request = require('supertest');
+
+describe('ONG',() => {
+    it('should be able to create a new ONG',() => {
+        ''
+    })
+})
